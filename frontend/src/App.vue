f20a6ccc81998f7ea58b5f0c26e2a8f5c99800df
AxisMac <erlia@naver.com>
2025-02-05 20:54:26 +0900

    First

---
+++ frontend/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="app">
+    <h1>{{ state.message }}</h1>
+    <div v-if=state.login>
+      <h1>Logged in</h1>
+
+    </div>
+  </div>
+
+</template>
+
+
+<script>
+import { reactive } from 'vue'
+
+export default {
+  setup() {
+    const state = reactive({
+      message: 'Hello Vue 3!',
+      login:false,
+    })
+
+    return { state }
+    
+  },
+}
+</script>
