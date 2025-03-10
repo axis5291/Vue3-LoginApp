c07b25878bb7f9565355e9eb2a4ac5535eb5003f
Axis <erlia@naver.com>
2025-03-10 23:09:19 +0900

    8.추가수정-1

--- backend/api-server.js
+++ backend/api-server.js
@@ -1,18 +1,18 @@
 
 const express = require('express');  
-//const cors = require('cors');                // 🚀 ##별도로 했음 CORS에러(같은 도메인명을 써서를 방지하기 위해 추가 
-const bodyParser = require('body-parser');   //body-parser는 클라이언트에서 req.body에 실어 보낸 JSON 등의 데이터를 Express에서 해석하고 사용할 수 있도록 변환해 주는 미들웨어이다.
+//const cors = require('cors');                 // 🚀 ##별도로 했음 CORS에러(같은 도메인명을 써서를 방지하기 위해 추가 
+const bodyParser = require('body-parser');      //body-parser는 클라이언트에서 req.body에 실어 보낸 JSON 등의 데이터를 Express에서 해석하고 사용할 수 있도록 변환해 주는 미들웨어이다.
 const cookieParser = require('cookie-parser');  // ***🚀 쿠키를 사용하기 위해 추가
-const jwt=require('jsonwebtoken');  // ***🚀 JWT를 사용하기 위해 추가
+const jwt=require('jsonwebtoken');              // ***🚀 JWT를 사용하기 위해 추가
 
 const app = express();
 const port = 3000;
-const jwtKey="abc1234567";  //***🚀 JWT를 사용하기 위해 추가
+const jwtKey="abc1234567";      //***🚀 JWT를 사용하기 위해 추가
 
-//app.use(cors());  // ##별도로 했음 ✅ CORS 설정 추가
+//app.use(cors());             // ##별도로 했음 ✅ CORS 설정 추가
 app.use(bodyParser.json());    //json데이터를 사용할 수 있도록 함
-app.use(cookieParser());  // ***🚀 쿠키를 사용하기 위해 추가
-//app.use(jwt());  // ***🚀 JWT를 사용하기 위해 추가
+app.use(cookieParser());       // ***🚀 쿠키를 사용하기 위해 추가
+//app.use(jwt());             // ***🚀 JWT를 사용하기 위해 추가
 
 const members=[
   { 
@@ -32,25 +32,29 @@ const members=[
 //암호화된 키는 임의로 지정하면 된다.
 
 app.get('/api/account', (req, res) => {
-  if(req.cookies && req.cookies.token){  //쿠키사용시:req.cookies.account 쿠키에 데이터가 있는지 확인
-     jwt.verify(req.cookies.token, jwtKey, (err, decoded)=>{  //verify메서드를 사용하여 토큰을 해독, 1인자(토큰), 2인자(암호화된 키), 3인자(콜백함수)
+   if(req.cookies && req.cookies.token){  //쿠키사용시:req.cookies.account 쿠키에 데이터가 있는지 확인
+      jwt.verify(req.cookies.token, jwtKey, (err, decoded)=>{  //verify메서드를 사용하여 토큰을 해독, 1인자(토큰), 2인자(암호화된 키), 3인자(콜백함수)
          if(err){
           console.log("콜백함수 에러내용 :",err);
           return res.send('아이디와 암호가 잘못되었습니다.');   //**return문을 만나면 함수를 빠져나오기 때문에 아래코드의 res.send()가 여러번 호출되지 않는다.
          }
-      return res.send(decoded);   
+        return res.send(decoded);   
       //*verify의 3번째인자 콜백함수의 2번재 인자에 해독된 데이터가 들어간다. 에러가 없으면 해독된 데이터를 클라이언트에 보냄
       })
     }
-     //*** 쿠키사용시 
-    // const member = JSON.parse(req.cookies.account); //쿠키에 저장된 데이터를 가져올 때는 JSON.parse()메서드를 사용하여 객체로 변환, 
-    // 
-    //  if(member.id){
-    //   return res.send(member);
-    //  }
-    else{   //
+     /* 쿠키사용시 
+
+      const member = JSON.parse(req.cookies.account); //쿠키에 저장된 데이터를 가져올 때는 JSON.parse()메서드를 사용하여 객체로 변환, 
+      
+      if(member.id){
+        return res.send(member);
+      }
+
+    */
+
+    else{   
       return res.send('로그인을 하세요');  //최초 접속을 할 때 보내는 기본 메세지
-   }
+    }
   })
 
 app.post('/api/account', (req, res) => {
--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,25 +1,23 @@
 <template>
   <div class="app">
     <h1>{{ state.message }}</h1>
-    <div v-if="state.account.id">
+    <div v-if="state.account.id">  <!-- 처음페이지 접속시 state.account.id에 값이 있으면 로그인 상태로 간주 -->
+       
       <h1>
         <p>안녕하세요? <span style="color: blue">{{ state.account.name }}!</span>님 로그인 하셨습니다.</p>
         <button @click="logout">로그아웃</button>
       </h1>
     </div>
 
-    <div v-else>
+    <div v-else>                   <!-- 처음페이지 접속시 state.account.id에 값이 없으면 아래것 실행 -->
       <label for="loginId"></label>
       <span>아이디</span>
-      <input type="text" name="" id="loginId" v-model="state.form.loginId" />
+      <input type="text" name="" id="loginId" v-model="state.form.loginId" />  <!-- v-model은 양방향 바인딩 -->
 
       <label for="loginPassword"></label>
       <span>패스워드</span>
       <input
-        type="password"
-        name=""
-        id="loginPassword"
-        v-model="state.form.loginPassword"
+        type="password" name="" id="loginPassword" v-model="state.form.loginPassword"
       />
       <hr />
       <button @click="submit">로그인</button>
@@ -48,28 +46,25 @@ export default {
     }); //state
 
     axios.get("/api/account").then((res) => {
-      state.account = res.data; // ***로그인 상태 유지
+      state.account = res.data;               // ***로그인 상태 유지
       alert("서버에서 보낸 메세지 : "+res.data.name+"님 환영합니다.");
      
     }); //axios.get
 
-    const submit = () => {
-      //로그인 버튼 클릭시 실행되는 함수, axis.post로 서버로 데이터 전송
-      const args = {
-        //로그인을 하게 되면 로그인 정보를 보내주는 객체
+    const submit = () => {        //로그인 버튼 클릭시 실행되는 함수, axis.post로 서버로 데이터 전송
+      const args = {              //form에 입력된 데이터를 args에 저장
         loginId: state.form.loginId,
         loginPassword: state.form.loginPassword,
       };
       console.log("서버로 보내는 데이터 사전 검증->", args);
 
       axios
-        .post("/api/account", args)
-        .then((res) => {
-        //  alert("로그인 버튼 누른후 서버에서 받은 데이터 : "+res.data.name);
-          state.account = res.data;
+        .post("/api/account", args)  //로그인 정보가 저장된 args를 서버로 전송
+        .then((res) => {             // 전송애 성공하고 나면 서버에서 보내온 데이터를 state.account에 저장
+          state.account = res.data;  //로그인 성공시 서버에서 받은 데이터를 state.account에 저장
           console.log("로그인 버튼 누른후 서버에서 받은 데이터 :", res.data);
         })
-        .catch((err) => {
+        .catch((err) => {            //전송에 실패하면 에러메세지 출력
           alert("로그인 실패");
           console.log("로그인 정보 서버로부터 받은 데이터:", err);
         }); //axios.post
@@ -82,7 +77,7 @@ export default {
       }); 
     }; //logout
 
-    return { state, submit, logout};
+    return { state, submit, logout}; 
   }, //setup   함수를 변수에 할당하는 것처럼 사용하면 ;이 필요하지만, 함수를 정의하는 것처럼 사용하면 ;이 필요없다.
 };
 </script>
